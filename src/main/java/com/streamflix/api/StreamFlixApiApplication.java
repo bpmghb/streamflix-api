ea7f3f947addaf5afc46516ba36b82253b832c92
Pedro <informatica@bpm.com.br>
2025-06-13 13:17:48 -0300

    Initial commit

---
+++ src/main/java/com/streamflix/api/StreamFlixApiApplication.java
@@ -0,0 +1,13 @@
+package com.streamflix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamFlixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamFlixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streamflix/api/StreamFlixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamflix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamFlixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
